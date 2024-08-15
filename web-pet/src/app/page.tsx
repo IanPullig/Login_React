db2fdfacdcae3d0675c66c01a155c474b350b1b6
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-08-15 09:01:48 -0400

    alterando o titulo do login

--- web-pet/src/app/page.tsx
+++ web-pet/src/app/page.tsx
@@ -1,4 +1,5 @@
 import { FormLogin } from '@/components/form-login'
+import { PawPrint } from 'lucide-react'
 
 export default function Login() {
   return (
@@ -9,7 +10,12 @@ export default function Login() {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Autenticação</h1>
+            <div className="flex items-center justify-center gap-3 ">
+              <div className="rounded-full bg-primary h-9 w-9 grid justify-center items-center text-primary-foreground">
+                <PawPrint className="h-4 w-4 transition-all group-hover:scale-110" />
+              </div>
+              <h1 className="text-3xl font-bold">e-CamPet</h1>
+            </div>
             <p className="text-balance text-muted-foreground">
               Insira suas credenciais abaixo para fazer entrar em sua conta
             </p>
