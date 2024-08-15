56f91f2b92789e4bbc8ea6190e895bef2f9ca08e
Gustavo Guilherme Campelo Antunes <guguiantunes@hotmail.com>
2024-08-15 10:12:53 -0400

    projeto base da api em java

---
+++ api-pet/src/test/java/com/desis/disa/apipet/ApiPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desis.disa.apipet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
